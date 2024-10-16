a9ec3c71fe115abb29e795595bfdf2664b7c6066
tamiresneves1711 <tamiresneves1711@gmail.com>
2024-10-16 16:59:10 -0300

    novo3

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let posY = 50;
 const moveSpeed = 50;
 const originalBackgroundColor = 'rgb(23, 85, 87)';
 
-document.addEventListener('keydown', (event) => {
-    switch (event.code) {
+document.addEventListener('keydown', (event)=>{
+    switch (event.code){
         case 'ArrowUp':
             if (posY > 0)
                 posY = posY - moveSpeed;
@@ -17,7 +17,7 @@ document.addEventListener('keydown', (event) => {
             if (posY < window.innerHeight - circle.offsetHeight)
                 posY = posY + moveSpeed;
             break;
-        case "Arrowleft":
+        case "ArrowLeft":
             if (posX > 0)
                 posX = posX - moveSpeed;
             break;
