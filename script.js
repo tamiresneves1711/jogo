bf75aa7c7569244822d22594ac7f87e04f41a4f6
tamiresneves1711 <tamiresneves1711@gmail.com>
2024-10-16 16:47:18 -0300

    novo2

--- script.js
+++ script.js
@@ -7,20 +7,29 @@ let posY = 50;
 const moveSpeed = 50;
 const originalBackgroundColor = 'rgb(23, 85, 87)';
 
-document.addEventListener('keydown', (event)=>{
-    switch(event.code){
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
         case 'ArrowUp':
             if (posY > 0)
                 posY = posY - moveSpeed;
             break;
         case 'ArrowDown':
-            if (posY < window.innerHeight - circle.offsetHeight) 
-                posY = posY+ moveSpeed;
+            if (posY < window.innerHeight - circle.offsetHeight)
+                posY = posY + moveSpeed;
             break;
-            case "Arrowleft":
-                if (posX > 0)
-                    posX = posX-moveSpeed;
-                break;
-        
+        case "Arrowleft":
+            if (posX > 0)
+                posX = posX - moveSpeed;
+            break;
+        case 'ArrowRight':
+            if (posX < window.innerWidth - circle.offsetWidth)
+                posX = posX + moveSpeed;
+            break;
     }
-})
+    updateCirclePosition();
+});
+
+function updateCirclePosition(){
+    circle.style.left = `${posX}px`;
+    circle.style.top = `${posY}px`;
+}
